473edaf2fee943192583f0cce921b55eb52d9b10
Julia-chaban <chaban79@bk.ru>
2025-08-08 14:00:24 +0300

    проблема с отправкой комментария

--- Work.js-main/index.js
+++ Work.js-main/index.js
@@ -4,61 +4,23 @@ import { handleClick, handleLikeClick } from "./clickHand.js";
 
 renderComments(commentsData);
 async function loadComments() {
-  fetch("https://wedev-api.sky.pro/api/v1/julia-chaban/comments", {
-    method: "GET",
-  })
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      renderComments(commentsData);
-    });
-}
+  try {
+    const response = await fetch(
+      "https://wedev-api.sky.pro/api/v1/julia-chaban/comments"
+    );
 
-async function newComment(name, text) {
-  fetch("https://wedev-api.sky.pro/api/v1/julia-chaban/comments", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ name: name, text: text }),
-  })
-    .then((response) => {
-      if (!response.ok) throw Error("Не удалось отправить комментарий");
-      return response.json();
-    })
-    .then((data) => {
-      console.log("Комментарий успешно отправлен!", data);
-      alert("Комментарий успешно отправлен!");
-      newComment();
-    })
-    .catch((error) => {
-      console.error("Ошибка:", error.message);
-      alert("Ошибка при отправке комментария");
-    });
-}
-window.onload = () => {
-  loadComments();
-};
-document.querySelector(".comments").addEventListener("click", (event) => {
-  handleClick(event);
-});
+    if (!response.ok) {
+      throw new Error(`Ошибка загрузки комментариев (${response.status})`);
+    }
 
-document.querySelector(".comments").addEventListener("click", (event) => {
-  handleLikeClick(event, commentsData);
-});
-
-document.querySelector(".add-form").addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  const name = document.querySelector(".add-form-name").value.trim();
-  const text = document.querySelector(".add-form-text").value.trim();
-
-  if (!name || !text) {
-    alert("Заполните поля.");
-    return;
+    const data = await response.json();
+    renderComments(data.comments);
+  } catch (error) {
+    console.error(error.message);
+    alert("Не удалось загрузить комментарии");
   }
-
+}
+async function saveNewComment(name, text) {
   const newComment = {
     name,
     text,
@@ -66,10 +28,52 @@ document.querySelector(".add-form").addEventListener("submit", (event) => {
     likesCount: 0,
     createdAt: new Date().toLocaleString(),
   };
+  try {
+    const response = await fetch(
+      "https://wedev-api.sky.pro/api/v1/julia-chaban/comments",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ comment: newComment }),
+      }
+    );
+    if (!response.ok) {
+      throw new Error(`Ошибка отправки комментария (${response.status})`);
+    }
+    alert("Комментарий успешно отправлен!");
+    loadComments();
+  } catch (error) {
+    console.error(error.message);
+    alert("Ошибка при отправке комментария");
+  }
+}
 
-  commentsData.push(newComment);
-  renderComments(commentsData);
+window.onload = async () => {
+  await loadComments();
+  document.querySelector(".comments").addEventListener("click", (event) => {
+    handleClick(event);
+  });
 
-  document.querySelector(".add-form-name").value = "";
-  document.querySelector(".add-form-text").value = "";
-});
+  document.querySelector(".comments").addEventListener("click", (event) => {
+    handleLikeClick(event, commentsData);
+  });
+
+  document.querySelector(".add-form").addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector(".add-form-name").value.trim();
+    const text = document.querySelector(".add-form-text").value.trim();
+
+    if (!name || !text) {
+      alert("Заполните все поля.");
+      return;
+    }
+
+    saveNewComment(name, text);
+
+    document.querySelector(".add-form-name").value = "";
+    document.querySelector(".add-form-text").value = "";
+  });
+};
